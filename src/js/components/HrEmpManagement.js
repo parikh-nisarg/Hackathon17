05b2a8228d7ccd0e0bd5e8d553061c318689afbc
Jignesh <jignesh@cakewalk.in>
2017-05-28 01:08:19 +0530

    Hr Management

---
+++ src/js/components/HrEmpManagement.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default class HrEmpManagement extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>HrEmpManagement
+            </div>
+
+        )
+    }
+
+}
---
+++ src/js/components/HrJobPosting.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default class HrJobPosting extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>Hr JobPosting
+            </div>
+
+        )
+    }
+
+}
