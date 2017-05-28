0af63817e81a286ca417fe8b329818e7abf32a19
sohel vali <sohel@cakewalk.in>
2017-05-28 18:20:00 +0530

    Carrer static

---
+++ src/js/components/Carrers.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Carrers extends React.Component{
+    constructor(){
+        super();
+    }
+
+    render(){
+        return(
+            <div>
+
+
+{/*new*/}
+    <div
+        style={{'margin':'35px','border':'1px solid #e8dddd', 'borderRadius': '8px'}}>
+        
+
+
+                
+                <div className="row">
+                    <div className="col-md-2">
+                        <img src="src/images/jobSearch.jpg" style={{'width':'150px'}}/>
+                    </div> 
+                        <div className="col-md-10">
+                            <div className="row" style={{'paddingBottom':'6px'}}>
+                                <div className="col-md-10">
+                                        <span style={{'fontSize':'20px'}}>
+                                            <strong>
+                                                Lorem ipsum dolor sit amet, consectetur
+                                                adipisicing elit.
+                                                &nbsp;&nbsp;
+                                            </strong>
+                                        </span>
+                                </div>    
+                                
+                                <div className="col-md-2">
+                                                <span
+                                                className="label label-info" style={{'fontSize':'12'}}>Full Time</span>
+                                                &nbsp;&nbsp;<span
+                                                className="label btnbrowse"
+                                                style={{'cursor':'pointer','fontSize':'12'}}>Apply</span>
+                                </div>    
+                            </div>
+                            
+                            <div className="row">
+                                <div className="col-md-12">
+                                    asasdasdasd asd asd asdfn kjasdjkas d kdjfkj hdf sdkhfkjh hsdfkhjh sdjkfkh sdkhfj sdfk df sdljkf sdfl df
+                                    alsddlj najklj asddfjk fdjkkj sdjkfjk jsdjfkjl jsjdfjjl sdf sdfuo ujsdfujh usdfuo osdfou sdofo osdf end
+                                    asd asdlj nasdjkbndkhkh hkaskgdhk khasdhkghk assdghjk ashjkdgahj dshgdhashdj asd
+                                </div>
+                            </div> 
+                            
+                            <div className="row" style={{'paddingTop':'14px'}}>
+                                <div className="col-md-4">
+                                    <span style={{'color':'grey'}}>Location : PUNE</span>&nbsp;&nbsp;
+                                    
+                                </div>
+                                <div className="col-md-4">
+                                <span style={{'color':'grey'}}>Skills  : ASP.NET, JQuery</span>
+                                </div>
+                                
+                            </div>       
+                    </div>    
+                    </div>
+                </div> 
+                                            {/*End new*/}    
+                                              {/*new*/}
+
+
+
+            </div>
+        )
+    }
+}
+
