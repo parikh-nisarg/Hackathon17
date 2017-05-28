db9f4b2c9822380096b63eb131e5d686fe912454
sohel vali <sohel@cakewalk.in>
2017-05-28 20:03:08 +0530

    Profile design updated

--- src/js/components/Profile.js
+++ src/js/components/Profile.js
@@ -28,13 +28,25 @@ export default class Profile extends React.Component {
         var data = this.state.userInfo;
         return (
             <div>
-               <img src="src/images/userprofile.png" />
-               { data.name}
-                { data.contact}
-                 { data.address}
-                  { data.email}
-               
-            </div>
+                  
+                    <div className="container" style={{'marginTop':'45px'}}>
+                        <div className="row">
+                            <div className="col-md-12" style={{'textAlign':'center'}}>
+                                <img src="src/images/userprofile.png" style={{'width':'300px','height':'300px'}} className="img-circle"/>             
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-12" style={{'textAlign':'center'}}>
+                                <div><b>Name :</b> { data.name}</div>
+                                <div><b>Address :</b> { data.address}</div>
+                                <div><b>Email :</b> { data.name}</div>
+                                <div><b>Contact :</b> { data.contact}</div>
+                                <div><b>Skills :</b> { data.skills}</div>
+                            </div>
+                        </div>
+                    </div>
+             </div>
+       
 
         )
     }
